1a1a088b0d55d0be65a1f5b98b4f53b47ded4729
zicohl <zicohl@gmail.com>
2022-01-17 23:02:48 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React, { PureComponent } from "react"
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
 import sendRequest from "@/util/axios"
+import Login from "@/pages/Login"
 
 export default class App extends PureComponent<{}, {}> {
     componentDidMount() {
@@ -7,10 +14,14 @@ export default class App extends PureComponent<{}, {}> {
     }
 
     render() {
-        return <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
+        return <BrowserRouter>
+            <div>
+                <h1>Welcome to React Router!</h1>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                </Routes>
+            </div >
+        </BrowserRouter>
+
     }
 }
